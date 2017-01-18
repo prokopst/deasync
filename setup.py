75c0f05e4465dd25165bc85e8c2408e0b86af9d5
Stanislav Prokop <prokopst.git@outlook.com>
2017-01-18 15:56:00 +0100

    Added reading of README.rst from setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup
 import sys
+from os import path
 
 if sys.version_info < (3, 5):
     raise RuntimeError("Python < 3.5 is not supported!")
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst')) as file:
+    long_description = file.read()
 
 setup(
     name='deasync',
     version='0.1.0',
     description="deasync decorator to make async functions/methods synchronous.",
+    long_description=long_description,
     url='https://github.com/prokopst/deasync',
     py_modules=['deasync'],
-    author='Stanislav Prokop',
+    author="Stanislav Prokop",
     license='MIT',
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -22,5 +27,6 @@ setup(
         "Programming Language :: Python :: 3.5",
     ],
     keywords="async await deasync desynchronize",
-    test_suite="tests",
+    test_suite='tests',
+    setup_requires=['nose']
 )
